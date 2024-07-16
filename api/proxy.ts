61b4b729919eb2da74ebc9babdd5e72f4a9e52cb
cch137 <96408836+cch137@users.noreply.github.com>
2024-07-16 08:04:10 +0800

    fix: cors

--- api/proxy.ts
+++ api/proxy.ts
@@ -4,6 +4,22 @@ export default async function handler1(
   req: VercelRequest,
   res: VercelResponse
 ) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const [input, init] = typeof req.body === "string" ? [req.body] : req.body;
   if (typeof input !== "string") return res.status(400).end();
   if (init && typeof init !== "object") return res.status(400).end();
@@ -16,16 +32,6 @@ export default async function handler1(
       if (/set-cookie/i.test(k)) k = `proxy-${k}`;
       res.appendHeader(k, v);
     });
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
     const reader = proxyRes.body?.getReader();
     if (!reader) return res.end();
     while (true) {
