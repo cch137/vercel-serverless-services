328b07e19013871b54f82e509d2fbf20c41769db
cch137 <96408836+cch137@users.noreply.github.com>
2024-07-19 08:44:53 +0800

    fix return value

--- api/currency/list.ts
+++ api/currency/list.ts
@@ -2,5 +2,5 @@ import { getCurrencyList } from "../../services/currency";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export async function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json(await getCurrencyList());
+  res.json(await getCurrencyList());
 }
