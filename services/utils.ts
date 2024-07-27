6977ea1138cf530164e524405551bdb1fb01fd6d
cch137 <96408836+cch137@users.noreply.github.com>
2024-07-27 13:59:18 +0800

    update utils

--- services/utils.ts
+++ services/utils.ts
@@ -1,7 +1,9 @@
-import { VercelRequest } from "@vercel/node";
-
 export function Booleanish(value: any) {
   if (typeof value !== "string") return Boolean(value);
   if (value === "false" || value === "0") return false;
   return true;
+}
+
+export function toSafeFilename(value: string) {
+  return value.replace(/[\/:*?"<>|\\\x00-\x1F]/g, "_");
 }
